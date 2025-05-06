887dede66097dbf615b8dbd10c8d7f68bb065ad5
rituraj-gharat <riturajgharat.14@gmail.com>
2025-05-06 00:56:42 -0700

    fix: updated superbase

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,10 @@
+// nuxt.config.ts
 export default defineNuxtConfig({
+  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/supabase'],
   runtimeConfig: {
     public: {
       supabaseUrl: process.env.NUXT_PUBLIC_SUPABASE_URL,
-      supabaseAnonKey: process.env.NUXT_PUBLIC_SUPABASE_ANON_KEY,
-    },
-  },
-});
+      supabaseAnonKey: process.env.NUXT_PUBLIC_SUPABASE_ANON_KEY
+    }
+  }
+})
