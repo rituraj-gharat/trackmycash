255dd20c4f4834ef4d839b0126f1cf457d9cf939
rituraj-gharat <riturajgharat.14@gmail.com>
2025-05-06 01:07:09 -0700

    Fix: correct env variable names for Vercel

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,8 @@
-// nuxt.config.ts
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/supabase'],
   runtimeConfig: {
     public: {
-      supabaseUrl: process.env.NUXT_PUBLIC_SUPABASE_URL,
-      supabaseAnonKey: process.env.NUXT_PUBLIC_SUPABASE_ANON_KEY
+      supabaseUrl: process.env.SUPABASE_URL,
+      supabaseAnonKey: process.env.SUPABASE_ANON_KEY,
     }
   }
-})
+});
