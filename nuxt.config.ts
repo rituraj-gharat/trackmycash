2cf1dbb331fe7b1503b22a3e8c3e3e82d0785a78
rituraj-gharat <riturajgharat.14@gmail.com>
2025-05-07 18:03:40 -0700

    Remove custom Supabase plugin to fix  error

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,4 @@
 export default defineNuxtConfig({
   modules: ['@nuxtjs/tailwindcss', '@nuxtjs/supabase'],
   css: ['@/assets/css/tailwind.css'],
-  supabase: {
-    redirect: false // Disable auth redirects
-  }
 })
